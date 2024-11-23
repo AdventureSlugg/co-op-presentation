adf17f49774040b295c322546736107fc7ee5973
Zoe <zmb6893@g.rit.edu>
2024-11-23 11:22:11 -0500

    Added in landing section for presentation

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import "@/styles/globals.css";
+import LandingSection from "./sections/landingSection";
+import BreakSection from "./sections/breakSection";
+
+export default function Home() {
+  return (
+    <div>
+      <LandingSection></LandingSection>
+      <BreakSection></BreakSection>
+    </div>
+  );
+}
---
+++ src/pages/sections/breakSection.tsx
@@ -0,0 +1,10 @@
+import '@/styles/break-section.css'
+
+
+export default function BreakSection () {
+	return (
+		<div className="break-section">
+
+		</div>
+	)
+}
